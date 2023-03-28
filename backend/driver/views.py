8457206709a4cdea060bda12babe52d04071a520
Daniel Houlihan <dhoulihan@indeed.com>
2023-03-28 23:11:19 +0100

    Adding functionaility for years in results

--- backend/driver/views.py
+++ backend/driver/views.py
@@ -16,10 +16,16 @@ class ResultView(viewsets.ModelViewSet):
     queryset = Result.objects.all()
 
     def get_queryset(self):
-        race_name = self.request.query_params.get('race')
-        if race_name:
+        race_name = self.request.query_params.get('race_name')
+        year = self.request.query_params.get('year')
+        if race_name and year:
+            return self.queryset.filter(race__race_name=race_name, race__season=year)
+        elif race_name:
             return self.queryset.filter(race__race_name=race_name)
+        elif year:
+            return self.queryset.filter(race__season=year)
         return self.queryset
+
 
 class RaceView(viewsets.ModelViewSet):
     serializer_class = RaceSerializer
