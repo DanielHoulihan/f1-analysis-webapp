04e84affd7764b07aac2fe1824560abc0e816910
Daniel Houlihan <dhoulihan@indeed.com>
2023-04-01 15:13:31 +0100

    frontend implementation of year selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import './App.css';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import RaceResultsTable from './RaceResultsTable'
+import axios from "axios";
+import RaceResultsTable from "./RaceResultsTable";
+import "./App.css";
 
 function Home() {
   return <h1>Welcome to the home page!</h1>;
@@ -11,120 +11,140 @@ function Home() {
 function App() {
   const [resultList, setResultList] = useState([]);
   const [raceList, setRaceList] = useState([]);
+  const [selectedYear, setSelectedYear] = useState("");
   const [selectedRace, setSelectedRace] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("/api/races/")
-      .then((res) => {
-        const races = res.data;
-        console.log(races)
-        setRaceList(races);
-        setSelectedRace(races[0].race_name); // set the first race as selected
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  
-  useEffect(() => {
-    if (selectedRace) {
-      axios
-        .get(`/api/results/?race=${selectedRace}`)
-        .then((res) => {
-          const resultList = res.data;
-          console.log(resultList);
-          setResultList(resultList);
-        })
-        .catch((err) => console.log(err));
+  const fetchRaces = async () => {
+    try {
+      const res = await axios.get("/api/races/");
+      setRaceList(res.data);
+      setSelectedRace(res.data[0].race_name);
+      setSelectedYear(res.data[0].season);
+    } catch (err) {
+      console.log(err);
     }
-  }, [selectedRace]);
-  
+  };
+
+  // Fetch race list on component mount
+  useEffect(() => {
+
+    fetchRaces();
+  }, []);
+
+  const fetchResults = async () => {
+    try {
+      const res = await axios.get(`/api/results/?race=${selectedRace}&year=${selectedYear}`);
+      setResultList(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // Fetch results on selected year and race change
+  useEffect(() => {
+
+    if (selectedRace && selectedYear) {
+      fetchResults();
+    }
+  }, [selectedRace, selectedYear]);
+
+  // Group results by race
+  const races = resultList.reduce((acc, result) => {
+    const raceName = result.race.race_name;
+    if (!acc[raceName]) {
+      acc[raceName] = [];
+    }
+    acc[raceName].push(result);
+    return acc;
+  }, {});
+
+  // Generate year options for select input
+  const yearOptions = Array.from(new Set(raceList.map((race) => race.season))).map((year) => (
+    <option key={year} value={year}>
+      {year}
+    </option>
+  ));
+
+  // Generate race options for select input
+  const raceOptions = raceList
+    .filter((race) => race.season === parseInt(selectedYear))
+    .map((race) => (
+      <option key={race.race_name} value={race.race_name}>
+        {race.race_name}
+      </option>
+    ));
+
+  // Handle year select input change
+  const handleYearChange = async (event) => {
+    const newSelectedYear = event.target.value;
+    setSelectedYear(newSelectedYear);
+    const racesOfYear = raceList.filter((race) => race.season === parseInt(newSelectedYear));
+    if (racesOfYear.length > 0) {
+      setSelectedRace(racesOfYear[0].race_name);
+      const res = await axios.get(`/api/results/?race=${racesOfYear[0].race_name}&year=${newSelectedYear}`);
+      setResultList(res.data);
+    } else {
+      setSelectedRace("");
+      setResultList([]);
+    }
+  };
+
+  // Handle race select input change
   const handleRaceChange = (event) => {
     setSelectedRace(event.target.value);
   };
 
-  const races = {};
-  for (const result of resultList) {
-    const raceName = result.race.race_name;
-    if (!races[raceName]) {
-      races[raceName] = [];
-    }
-    races[raceName].push(result);
-  }
-
-  const raceOptions = raceList.map((race) => (
-    <option key={race.race_name} value={race.race_name}>
-      {race.race_name}
-    </option>
-  ));
-
   return (
     <BrowserRouter>
       <div>
-        <div className="nav">
+        <div className="navbar">
           <nav>
             <ul>
               <li>
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/analysis">Analysis</Link>
+                <Link to="/results">Results</Link>
+              </li>
+              <li>
+                <Link to="/standings">Standings</Link>
               </li>
             </ul>
           </nav>
         </div>
-        <div className="content" style={{ paddingLeft: '10%',paddingRight: '10%'  }}>
-        <Routes>
-          <Route
-            path="/analysis"
-            element={
-              <>
-                <select value={selectedRace} onChange={handleRaceChange}>
-                  {raceOptions}
-                </select>
-                <RaceResultsTable races={races} selectedRace={selectedRace} />
-              </>
-            }
-          />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div className="content" style={{ paddingLeft: "10%", paddingRight: "10%" }}>
+          <Routes>
+            <Route
+              path="/results"
+              element={
+                <>
+                  <div>
+                    <label htmlFor="yearSelect">Select year:</label>
+                    <select value={selectedYear} onChange={handleYearChange}>
+                      {yearOptions}
+                    </select>
+                  </div>
+                  {selectedYear && (
+                    <div>
+                      <label htmlFor="raceSelect">Select race:</label>
+                      <select id="raceSelect" value={selectedRace} onChange={handleRaceChange}>
+                        {raceOptions}
+                      </select>
+                    </div>
+                  )}
+                  {selectedRace && (
+                    <RaceResultsTable races={races} selectedRace={selectedRace} />
+                  )}
+                </>
+              }
+            />
+            <Route path="/" element={<Home />} />
+            <Route path="/standings" element={<Home />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
   );
-
-  function RaceResultsTable({ races, selectedRace }) {
-    const raceRows = Object.entries(races).map(([raceName, results]) => {
-      if (selectedRace && selectedRace !== raceName) {
-        return null;
-      }
-      return (
-        <React.Fragment key={raceName}>
-          <tr>
-            <td colSpan="10" className="race-header">
-              <h1>{raceName}</h1>
-              <p>{results[0].race.date}</p>
-            </td>
-          </tr>
-          <tr className="header">
-            <th></th>
-            <th>Driver</th>
-            <th>Constructor</th>
-            <th>Points</th>
-          </tr>
-          {results.map((result, index) => (
-          <tr key={result.id}>
-            <td>{index + 1}</td>
-            <td>{result.driver}</td>
-            <td>{result.constructor}</td>
-            <td>{result.points}</td>
-          </tr>
-        ))}
-        </React.Fragment>
-      );
-    });
-  
-    return <table><tbody>{raceRows}</tbody></table>;
-  }
   
 }
+
 export default App;
